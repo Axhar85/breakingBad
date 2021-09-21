0686fd65d1e091805ff7bec4c91c484d8d55b1d2
Axhar85 <azhararshad_ntu@yahoo.com>
2021-09-21 19:18:03 +0200

    p

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { useState} from 'react';
+import React, { useEffect, useState} from 'react';
 import './App.css';
 import Header from './components/Header';
+import List from './components/List';
 
 function App() {
-  const [title]= useState('Breaking Bad!')
+  const [title]= useState('Breaking Bad!');
+  let[data, setData]= useState([]);
+
+  useEffect(() => {
+    fetch('https://www.breakingbadapi.com/api/characters')
+      .then((response) => response.json())
+      .then(data => setData(data))
+  }, []);
+
   return (
     <div className="App">
     <Header title={title} />
+    <List characters={data}/>
     </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const List = (props) =>  <h1>List is getting ready</h1>
+    
+    
+
+export default List;
