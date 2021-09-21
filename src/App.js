9849a4204786c23857c6d18961fe9a254902de57
Axhar85 <azhararshad_ntu@yahoo.com>
2021-09-21 11:33:41 +0200

    create the pages we gonna use in app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React, { useState} from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+  const [title]= useState('Breaking Bad!')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header title={title} />
     </div>
   );
 }
